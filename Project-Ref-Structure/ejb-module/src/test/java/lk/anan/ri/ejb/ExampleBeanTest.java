eebb6972ea433c2cc6981f860f9507e64c207925
Anan <poornan@gmail.com>
2024-10-31 13:33:45 +0800

    lombok added

--- Project-Ref-Structure/ejb-module/src/test/java/lk/anan/ri/ejb/ExampleBeanTest.java
+++ Project-Ref-Structure/ejb-module/src/test/java/lk/anan/ri/ejb/ExampleBeanTest.java
@@ -4,13 +4,14 @@ import org.apache.openejb.testing.Module;
 import org.junit.jupiter.api.Test;
 
 import jakarta.ejb.EJB;
+import lombok.extern.slf4j.Slf4j;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.apache.openejb.jee.EjbJar;
 import org.apache.openejb.jee.StatelessBean;
 import org.apache.openejb.junit5.RunWithApplicationComposer;
-
+@Slf4j
 @RunWithApplicationComposer
 public class ExampleBeanTest {
 
@@ -28,5 +29,6 @@ public class ExampleBeanTest {
     public void sayHello() {
         String result = exampleBean.sayHello("Ananth");
         assertEquals("Hello, Ananth!", result);
+        log.info(result);
     }
 }
