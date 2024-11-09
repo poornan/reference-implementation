a2b6a4a2a78077a8aba2b9d47fa1949c36123abe
Anan <poornan@gmail.com>
2024-11-09 12:55:48 +0800

    created instrumentation module

---
+++ acceptance-test/instrument/src/test/java/lk/anan/ri/instrument/ExampleTest.java
@@ -0,0 +1,13 @@
+package lk.anan.ri.instrument;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ExampleTest {
+
+    @Test
+    public void testAddition() {
+        int sum = 1 + 1;
+        assertEquals(2, sum, "1 + 1 should equal 2");
+    }
+}
