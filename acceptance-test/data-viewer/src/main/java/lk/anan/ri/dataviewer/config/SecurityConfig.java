34502762286558daf21b9837746078afd3ac1e2c
Anan <poornan@gmail.com>
2024-11-10 22:34:37 +0800

    h2-console working

--- acceptance-test/data-viewer/src/main/java/lk/anan/ri/dataviewer/config/SecurityConfig.java
+++ acceptance-test/data-viewer/src/main/java/lk/anan/ri/dataviewer/config/SecurityConfig.java
@@ -7,12 +7,16 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer.FrameOptionsConfig;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.ldap.DefaultSpringSecurityContextSource;
 import org.springframework.security.ldap.authentication.LdapAuthenticationProvider;
 import org.springframework.security.ldap.authentication.BindAuthenticator;
 import org.springframework.security.ldap.search.FilterBasedLdapUserSearch;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.security.web.util.matcher.RequestMatcher;
+
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
@@ -22,22 +26,22 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http, LdapAuthenticationProvider authProvider)
             throws Exception {
-        http
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/css/**", "/js/**", "/images/**", "/webjars/**", "/error", "/h2-console/**").permitAll()
-                        .requestMatchers("/api/**").authenticated()
-                        .anyRequest().authenticated())
+        http.authorizeHttpRequests(auth -> auth
+                .requestMatchers("/css/**", "/js/**", "/images/**", "/webjars/**", "/error", "/h2-console/**")
+                .permitAll()
+                .requestMatchers("/api/**").authenticated()
+                .anyRequest().authenticated())
                 .formLogin(withDefaults())
                 .logout(logout -> logout
-                    .logoutUrl("/logout")
-                    .logoutSuccessUrl("/login?logout")
-                    .invalidateHttpSession(true)
-                    .deleteCookies("JSESSIONID")
-                )
+                        .logoutUrl("/logout")
+                        .logoutSuccessUrl("/login?logout")
+                        .invalidateHttpSession(true)
+                        .deleteCookies("JSESSIONID"))
                 .csrf(csrf -> csrf.disable())
                 .cors(withDefaults())
                 .authenticationProvider(authProvider)
-                .httpBasic(withDefaults());
+                .httpBasic(withDefaults())
+                .headers(headers -> headers.frameOptions(FrameOptionsConfig::disable));
 
         return http.build();
     }
@@ -64,5 +68,10 @@ public class SecurityConfig {
         authenticator.setUserSearch(userSearch);
         authenticator.afterPropertiesSet();
         return new LdapAuthenticationProvider(authenticator);
+    }
+
+    @SuppressWarnings("unused")
+    private static RequestMatcher toH2Console() {
+        return new AntPathRequestMatcher("/h2-console/**");
     }
 }//
